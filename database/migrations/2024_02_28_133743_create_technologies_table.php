0b99a06837d84e3c9ecadbcebce877db45328d8b
nicomanunta <nic.manun@gmail.com>
2024-02-28 14:41:46 +0100

    creata tabella e aggiunte colonne

--- database/migrations/2024_02_28_133743_create_technologies_table.php
+++ database/migrations/2024_02_28_133743_create_technologies_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50)->unique();
+            $table->string('slug', 100);
             $table->timestamps();
         });
     }
