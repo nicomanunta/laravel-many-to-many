4abac4d97df4a190cc2f0bba0e6f16a8adcdc5bb
nicomanunta <nic.manun@gmail.com>
2024-02-28 15:13:30 +0100

    creata tabella ponte e colonne

--- database/migrations/2024_02_28_134858_create_project_technology_table.php
+++ database/migrations/2024_02_28_134858_create_project_technology_table.php
@@ -14,8 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('project_technology', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('technology_id');
+            $table->foreign('technology_id')->references('id')->on('technologies')->cascadeOnDelete();
+
         });
     }
 
